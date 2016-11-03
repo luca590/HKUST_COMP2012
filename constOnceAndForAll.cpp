60f5810c55f1eda9569e5d11228cf094ec5842f8
luca590 <lucasgaylord@protonmail.com>
2016-11-03 23:38:52 +0800

    const update

--- constOnceAndForAll.cpp
+++ constOnceAndForAll.cpp
@@ -11,17 +11,18 @@ int otherVar;
 public:
 myClass(int a, int b):myVar(a), otherVar(b) {cout << "constructor myClass called" << endl;}
 
-void nonConstFunction(char* me) {cout << "nonConstFunction called *** " << endl; cout << "I am: " << me << endl;}
-void nonConstFunction(char& me) {cout << "nonConstFunction called &&& " << endl; cout << "I am: " << me << endl;}
-const int& constFunction(const int& a, int b) const {cout << "constFunction called" << endl; return a;}
-void constRef(const int& a, int b) {cout << "a is: " << a << endl; b++;} //cannot change a inside function, but may change other var.s
-const int& constReturn(int& a, int b) {cout << "constReturn called " << endl; a++; return a;}
+void nonConstFunction(char* me) {cout << "nonConstFunction called *** I am: " << me << endl;}
+void nonConstFunction(char& me) {cout << "nonConstFunction called &&& I am: " << me << endl;}
+const char* constFunction(const char* a) const {cout << "constFunction called" << endl; return a;}
+void constRef(const char* a) {cout << "constRef called, a is: " << endl; }//cannot change a inside function, but may change other var.s
+const char* constReturn(char* a) {cout << "constReturn called " << endl; return a;} 
  //cannot have global const function
 
 };
 
 
 int main() {
+cout << "\n\nLet the races begin!!\n" << endl;
 int a = 1;
 int b = 2;
 
@@ -36,17 +37,65 @@ const char *const cpcc = &c;
 char const *const cpcc2 = &c;
 
 myClass A(a, b);
-cout << "calling pointers " << endl;
+
+/************* char c = 'Y' *************/
+cout << endl << endl; cout << "char c = 'Y'" << endl;
+A.nonConstFunction(c);
+
+/************* char *const cpc = &c *************/
+cout << endl << endl; cout << "char *const cpc = &c" << endl;
 A.nonConstFunction(cpc);
-A.nonConstFunction(pcc);
-A.nonConstFunction(pcc2);
-A.nonConstFunction(cpcc);
-A.nonConstFunction(cpcc2);
+A.constRef(cpc);
+A.constReturn(cpc);
+A.constFunction(cpc);
+
+/************* char const* pcc *************/
+cout << endl << endl; cout << "char const * pcc" << endl;
+//A.nonConstFunction(pcc); ERROR
+A.constRef(pcc);
+//A.constReturn(pcc); ERROR
+A.constFunction(pcc);
+
+/************* const char* pcc2 *************/
+cout << endl << endl; cout << "const char* pcc2" << endl;
+//A.nonConstFunction(pcc2); ERROR
+A.constRef(pcc2);
+//A.constReturn(pcc2); ERROR
+A.constFunction(pcc2);
+
+/************* const char *const cpcc = &c *************/
+cout << endl << endl; cout << "const char *const cpcc = &c" << endl;
+//A.nonConstFunction(cpcc); ERROR
+A.constRef(cpcc);
+//A.constReturn(cpcc); ERROR
+A.constFunction(cpcc);
+
+/************* char const *const cpcc2 = &c *************/
+cout << endl << endl; cout << "char const *const cpcc2 = &c" << endl;
+//A.nonConstFunction(cpcc2); ERROR
+A.constRef(cpcc2);
+//A.constReturn(cpcc2); ERROR
+A.constFunction(cpcc2);
 
 
-A.constRef(a,b);
-A.constReturn(a, b);
-A.constFunction(a, b);
+
+
+
+//Now for a constant object
+const myClass B(3,4);
+
+cout << "calling pointers " << endl;
+/* //error "no matching member function to call for nonConstFunctio"
+B.nonConstFunction(cpc);
+B.nonConstFunction(pcc); 
+B.nonConstFunction(pcc2);
+B.nonConstFunction(cpcc);
+B.nonConstFunction(cpcc2);
+*/
+
+//B.constRef(3,4);//error
+//B.constReturn(3, 4); //error
+
 
 return 0;
 }
